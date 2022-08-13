22c5cc06a0ff5be62af482f7554cea18f62c56ca
Man980 <ralphmany91@gmail.com>
2022-08-13 19:30:14 -0400

    preparing new challenges

--- codelevel.py
+++ codelevel.py
@@ -275,9 +275,14 @@ for elem in split_string:
 
 print(" ".join(lis))
 
-# display duplicate numbers just for fun
+# display duplicate numbers
 def duplicateNumbers(nums):
 	nums.sort()
 	for i in range(1, len(nums)):
 		if nums[i]==nums[i-1]:
 			return nums[i]
+print(duplicateNumbers([1, 4, 3, 5, 3]))
+
+
+
+#preparing new challenges
